55f7b265022968d40338479bfc45c618f9d46a8f
ViniciusPelizzari <vinipelizzari2410@gmail.com>
2023-02-03 10:53:08 -0300

    Ajuste código

--- verificacao_senhas.py
+++ verificacao_senhas.py
@@ -7,19 +7,11 @@ print("Senha: ")
 senha_c = input()
 #verificacao_senhas
 
-#verdadeiro = True
-#print("Verdadeiro =", verdadeiro)
-#falso = False
-#print("Falso =", falso)
-#print(type(falso))
-#print(type(verdadeiro))
 if adm == adm_c and senha == senha_c:
   print("Acesso autorizado!")
 else:
   print("Usuário ou senha incorreta!")
 
-
-#verificacao_senhas
 adm = "Vinícius"
 senha = "24102003"
 confer = adm == adm_c and senha == senha_c
